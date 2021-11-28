33408945ac1befa76e0b2f8544484cffcbf875e8
Rens van de Schoot <a.g.j.vandeschoot@uu.nl>
2021-11-28 10:31:19 +0100

    update plug-in to process six datasets

--- asreviewcontrib/datasets_ftm/datasets_ftm.py
+++ asreviewcontrib/datasets_ftm/datasets_ftm.py
@@ -11,7 +11,7 @@ class YourDataGroup(BaseDataGroup):
         metadata_assen = BaseDataSet.from_config({
             "dataset_id": "data_assen_mockup",
             "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_assen_mockup.csv",
-            "img_url": "https://www.assen.nl/sites/all/themes/custom/assen/logo.png",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/assen.png",
             "link": "https://www.ftm.nl/dossier/shell-papers",
             "license": "",
             "title": "Assen (Mock-up)",
@@ -26,7 +26,7 @@ class YourDataGroup(BaseDataGroup):
         metadata_drenthe = BaseDataSet.from_config({
             "dataset_id": "data_drenthe_mockup",
             "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_drenthe_mockup.csv",
-            "img_url": "https://www.provincie.drenthe.nl/publish/pages/120213/logo-provincie-drenthe.png",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/drenthe.PNG",
             "link": "https://www.ftm.nl/dossier/shell-papers",
             "license": "",
             "title": "Drenthe (Mock-up)",
@@ -38,9 +38,73 @@ class YourDataGroup(BaseDataGroup):
         }
         )
 
+        
+        metadata_amsterdam = BaseDataSet.from_config({
+            "dataset_id": "data_amsterdam_mockup",
+            "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_amsterdam_mockup.csv",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/amsterdam.PNG",
+            "link": "https://www.ftm.nl/dossier/shell-papers",
+            "license": "",
+            "title": "Amsterdam (Mock-up)",
+            "authors": [
+            "FTM"
+            ],
+            "year": 2021,
+            "topic": ""
+        }
+        )
+
+
+        metadata_nh = BaseDataSet.from_config({
+            "dataset_id": "data_nh_mockup",
+            "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_nh_mockup.csv",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/nh.PNG",
+            "link": "https://www.ftm.nl/dossier/shell-papers",
+            "license": "",
+            "title": "Noord-Holland (Mock-up)",
+            "authors": [
+            "FTM"
+            ],
+            "year": 2021,
+            "topic": ""
+        }
+        )
+        
+        metadata_minfin = BaseDataSet.from_config({
+            "dataset_id": "data_minfin_mockup",
+            "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_minfin_mockup.csv",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/minfin.PNG",
+            "link": "https://www.ftm.nl/dossier/shell-papers",
+            "license": "",
+            "title": "Ministerie van Financien (Mock-up)",
+            "authors": [
+            "FTM"
+            ],
+            "year": 2021,
+            "topic": ""
+        }
+        )
+        
+        metadata_minienw = BaseDataSet.from_config({
+            "dataset_id": "data_minienw_mockup",
+            "url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/data/data_minienw_mockup.csv",
+            "img_url": "https://raw.githubusercontent.com/asreview-ftm-hackathon/ASReview-plugin-FTM/main/images/minienw.PNG",
+            "link": "https://www.ftm.nl/dossier/shell-papers",
+            "license": "",
+            "title": "Ministerie van Infrastructuur en Waterstaat (Mock-up)",
+            "authors": [
+            "FTM"
+            ],
+            "year": 2021,
+            "topic": ""
+        }
+        )
         assen = BaseVersionedDataSet("test1", [metadata_assen], "test2")
         drenthe = BaseVersionedDataSet("test3", [metadata_drenthe], "test4")      
+        amsterdam = BaseVersionedDataSet("test5", [metadata_amsterdam], "test6")
+        nh = BaseVersionedDataSet("test7", [metadata_nh], "test8")
+        minfin = BaseVersionedDataSet("test9", [metadata_minfin], "test10")
+        minienw = BaseVersionedDataSet("test11", [metadata_minienw], "test12")    
 
-
-        super(YourDataGroup, self).__init__(assen, drenthe)
+        super(YourDataGroup, self).__init__(assen, drenthe, amsterdam, nh, minfin, minienw)
         # pass multiple datasets to init if there are more datasets
